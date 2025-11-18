97ed830a34235f29756b973c131e51331c612978
abdi-20 <hernandezabdiel466@gmail.com>
2025-11-17 19:42:30 -0600

    Actualización

--- javascript/galeria-slider.js
+++ javascript/galeria-slider.js
@@ -1,25 +1,14 @@
-'use strict';
+"use strict";
 
-// Selecciona los botones
-let nextButton = document.querySelector('.galeria-buttons .next');
-let prevButton = document.querySelector('.galeria-buttons .prev');
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
 
-// Lógica para el botón 'Siguiente'
-nextButton.addEventListener('click', function(){
-    // Obtiene todos los items y el contenedor
-    let items = document.querySelectorAll('.slide .item');
-    let slideContainer = document.querySelector('.slide');
-    
-    // Mueve el primer item al final de la lista
-    slideContainer.appendChild(items[0]);
+next.addEventListener("click", function () {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").appendChild(items[0]);
 });
 
-// Lógica para el botón 'Anterior'
-prevButton.addEventListener('click', function(){
-    // Obtiene todos los items y el contenedor
-    let items = document.querySelectorAll('.slide .item');
-    let slideContainer = document.querySelector('.slide');
-    
-    // Mueve el último item al principio de la lista
-    slideContainer.prepend(items[items.length - 1]);
+prev.addEventListener("click", function () {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").prepend(items[items.length - 1]);
 });
